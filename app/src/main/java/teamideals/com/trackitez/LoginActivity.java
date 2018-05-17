2af7582bda4ea1796ebe39342b42ca5759e543b5
idl99 <ihandilnath2012@gmail.com>
2018-05-17 16:57:16 +0530

    Added main menu and login functionality

--- app/src/main/java/teamideals/com/trackitez/LoginActivity.java
+++ app/src/main/java/teamideals/com/trackitez/LoginActivity.java
@@ -3,6 +3,7 @@ package teamideals.com.trackitez;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -28,6 +29,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -49,7 +51,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
+            "admin@gmail.com:admin"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
@@ -185,7 +187,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
+            mAuthTask = new UserLoginTask(email, password, this);
             mAuthTask.execute((Void) null);
         }
     }
@@ -290,6 +292,24 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         int IS_PRIMARY = 1;
     }
 
+    public void goToMainActivity(){
+        // Method which navigates to the next screen
+        Intent intent = new Intent(this, MainActivity.class);
+        this.startActivity(intent);
+    }
+
+    public void resetFields(){
+        // Method to reset login form fields
+        this.mEmailView.setText("");
+        this.mPasswordView.setText("");
+    }
+
+    public void showErrorToast(){
+        // Method which shows error toast for invalid credentials
+        resetFields();
+        Toast.makeText(this, R.string.error_incorrect_credentials, Toast.LENGTH_SHORT).show();
+    }
+
     /**
      * Represents an asynchronous login/registration task used to authenticate
      * the user.
@@ -298,10 +318,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         private final String mEmail;
         private final String mPassword;
+        private final LoginActivity activity;
 
-        UserLoginTask(String email, String password) {
+        UserLoginTask(String email, String password, LoginActivity activity) {
             mEmail = email;
             mPassword = password;
+            this.activity = activity;
         }
 
         @Override
@@ -324,7 +346,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
 
             // TODO: register the new account here.
-            return true;
+            return false;
         }
 
         @Override
@@ -333,10 +355,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+                // Goes to a method which calls the next activity
+                activity.goToMainActivity();
             } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
+                // Shows error toast
+                activity.showErrorToast();
             }
         }
 
