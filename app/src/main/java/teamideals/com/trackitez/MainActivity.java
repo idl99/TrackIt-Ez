e030378e74a5f26b77958110e577ffd1cca93fdf
idl99 <ihandilnath2012@gmail.com>
2018-05-19 22:10:13 +0530

    - main activity overhaul

--- app/src/main/java/teamideals/com/trackitez/MainActivity.java
+++ app/src/main/java/teamideals/com/trackitez/MainActivity.java
@@ -4,15 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import teamideals.com.trackitez.databinding.ContentMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     private Intent intent;
 
@@ -32,16 +38,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         // Getting user details passed from LoginActivity to MainActivity
         intent = getIntent();
         User sessionUser = (User) intent.getExtras().getSerializable("user");
 
         /* Data Binding
-
          1. Declare layout binding variable in XML file: check content_main.xml
-
          2. Gradles generates the binding class for relevant layout file - ContentMainBinding
-
          3. Attach data binding to layout where binding variable declared, using
          inflate and set??? methods where ??? is the name of the binding variable declared
          in the layout file: setUser --> binding variable user in content_main.xml*/
@@ -49,11 +61,67 @@ public class MainActivity extends AppCompatActivity {
         ContentMainBinding contentMainBinding = ContentMainBinding.inflate(
                 getLayoutInflater(),(ViewGroup) findViewById(R.id.content_main_parent),true
         ); // Attach data binding to content_main_parent view
-           // of layout where binding variable declared
+        // of layout where binding variable declared
 
         contentMainBinding.setUser(sessionUser); // copy reference of sessionUser to user in
-                                                 // content_main.xml
+        // content_main.xml
 
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
