be7c5407b62c7f0d6a1b82994b4014bcc047c9e4
idl99 <ihandilnath2012@gmail.com>
2018-05-30 10:23:24 +0530

    - added ItemCategory ENUM

---
+++ app/src/main/java/teamideals/com/trackitez/entities/ItemCategory.java
@@ -0,0 +1,6 @@
+package teamideals.com.trackitez.entities;
+
+public enum ItemCategory {
+    BEVERAGE, BREAD, CANNED_GOODS, DAIRY, DRY_FOOD, BAKING_GOODS, FROZEN_FOOD, MEAT,
+    PRODUCE, PERSONAL_CARE, MEDICINE, BABY_PRODUCTS, OTHER
+}
